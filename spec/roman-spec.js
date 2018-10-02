a794025dc48b06b69691dc4327eddfa4094029a4
austin cummings and ben kiggen <austin@>
2018-10-02 16:47:53 -0700

    tests run

---
+++ spec/roman-spec.js
@@ -0,0 +1,81 @@
+import { Board } from './../src/sudoku.js';
+
+describe('BoardCheck', function() {
+  var testBoard = new Board([
+   [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+    [1,2,3,4,5,6,7,8,9],
+     [1,2,3,4,5,6,7,8,9],
+     [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+      [1,2,3,4,5,6,7,8,9],
+       [1,2,3,4,5,6,7,8,9]);
+
+  // var testBoard = new Board([3,9,2,3,3,4,5,6,6],
+    // [1,2,3,4,5,6,7,8,9],
+    // [2,2,2,5,8,1,2,2,2],
+    // [2,2,4,2,2,2,2,2,2],
+    // [5,1,7,2,2,2,9,2,2],
+    // [6,2,3,2,2,2,3,7,2],
+    // [1,2,2,8,2,4,2,2,2],
+    // [7,2,6,2,2,2,8,1,2],
+    // [3,2,2,2,9,2,2,2,2]);
+
+  it('should only contain integers from 1-9 in the first digit entered', function() {
+
+    expect(typeof testBoard.row6[2]).toEqual('number');
+  });
+
+  it('each row should only contain integers from 1-9 in the row entered', function() {
+
+      for(var j = 0; j < 9; j++) {
+        var rowNumber = ('row'+j);
+        var testRow = testBoard[rowNumber];
+        for(var i = 0; i < 9; i++) {
+          expect(typeof(testRow[i])).toEqual('number');
+          expect(testRow[i]).not.toBeLessThan(1);
+          expect(testRow[i]).toBeLessThan(10);
+      }
+    }
+  });
+
+  it('each row should have no more than one of each number, 1-9', function() {
+
+
+      for(var j = 0; j < 9; j++) {
+        var rowNumber = ('row'+j);
+        var testRow = testBoard[rowNumber];
+        // var repeatArray = [];
+        // for(var i = 0; i < 9; i++) {
+        //   repeatArray.push(testRow[i]);
+          var results = testRow.sort();
+          var control = [1,2,3,4,5,6,7,8,9];
+          for(var z = 0; z < 9; z++){
+          expect(results[z]).toEqual(control[z]);
+        }
+
+    }
+  });
+  //
+  // it('each column should have no more than one of each number, 1-9', function() {
+  //
+  //     for(var j = 0; j < 9; j++) {
+  //       var rowNumber = ('row'+j);
+  //       var testRow = testBoard[rowNumber];
+  //       for(var i = 0; i < 9; i++) {
+  //         expect(typeof(testRow[i])).toEqual('number');
+  //         expect(testRow[i]).not.toBeLessThan(1);
+  //         expect(testRow[i]).toBeLessThan(10);
+  //     }
+  //   }
+  //
+  //
+  // });
+
+
+
+
+
+
+
+});
--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/styles.scss';
 import './sudoku.js';
-import {backEnd} from './.js';
+import {backEnd} from './sudoku.js';
 import $ from 'jquery';
+
+
+$(document).ready(function(){
+  backEnd();
+});
